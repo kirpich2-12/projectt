43a80058e1b17a104293fe81b0ad3a30c15516dc
kirpich2-12 <olegovicmakar396@gmail.com>
2024-06-01 11:31:22 +0300

    project

---
+++ Assets/Scripts/CarForward.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarForward : MonoBehaviour
+{
+    public int speed = 20;
+
+
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime  * speed);
+    }
+}
